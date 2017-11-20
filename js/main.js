77fa5dee6ca6adc725d650371a64631651ec3c8c
SAM-LEV-T410\awassell <awassell@fcptech.com>
2017-11-20 17:00:33 +0000

    adding some layout UI

--- js/main.js
+++ js/main.js
@@ -108,17 +108,17 @@
             })
         .controller("weeklyChartCtrl",
             function ($scope) {
-                $scope.colors = ['#4286f4', '#4286f4', '#4286f4'];
+                $scope.colors = ['#45b7cd', '#ff6384', '#ff8e72'];
 
                 $scope.labels = ['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday', 'Sunday'];
                 $scope.data = [
-                    [65, 59, 80, 81, 56, 55, 40],
-                    [54, 23, 43, 56, 87, 26, 87]
+                    [65, -59, 80, 81, 56, 55, 40],
+                    [28, 48, 40, 19, 86, 27, 90]
                 ];
                 $scope.datasetOverride = [
                     {
                         label: "Bar chart",
-                        borderWidth: 2,
+                        borderWidth: 1,
                         type: 'bar'
                     },
                     {
@@ -351,7 +351,7 @@
             $scope.myChartObject.type = "Gauge";
 
             $scope.myChartObject.options = {
-                width: 400,
+                width: 200,
                 height: 120,
                 redFrom: 90,
                 redTo: 100,
@@ -371,7 +371,7 @@
             $scope.myChartObject.type = "Gauge";
 
             $scope.myChartObject.options = {
-                width: 400,
+                width: 200,
                 height: 120,
                 redFrom: 90,
                 redTo: 100,

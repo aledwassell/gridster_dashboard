9c294913e88a7c5d1844c427cb9d1c0580e9734b
SAM-LEV-T410\awassell <awassell@fcptech.com>
2017-11-20 17:39:14 +0000

    adding some layout UI

--- js/main.js
+++ js/main.js
@@ -37,6 +37,9 @@
                 })
                 .when("/layout", {
                     templateUrl: 'views/layout.html'
+                })
+                .when("/layout02", {
+                    templateUrl: 'views/layout02.html'
                 });
             dashboardProvider
                 .structure('6-6', {
@@ -404,6 +407,11 @@
                 ['Label', 'Value'],
                 ['National', 30]
             ];
+        })
+        .controller("DoughnutCtrl", function ($scope) {
+            $scope.colors = ['#45b7cd', '#ff6384', '#ff8e72'];
+            $scope.labels = ["Download Sales", "In-Store Sales", "Mail-Order Sales"];
+            $scope.data = [300, 500, 100];
         })
         .directive('gaugeDirective', function(){
             return {

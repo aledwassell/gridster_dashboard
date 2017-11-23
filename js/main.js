f21ab963317ea91b54b5de71fcf356d666b0a1cf
SAM-LEV-T410\awassell <awassell@fcptech.com>
2017-11-23 17:24:01 +0000

    adding some layout UI

--- js/main.js
+++ js/main.js
@@ -48,6 +48,10 @@
                 .when("/layout04", {
                     templateUrl: 'views/layout04.html',
                     controller: 'layout04'
+                })
+                .when("/possible_charts", {
+                    templateUrl: 'views/charts.html',
+                    controller: 'charts'
                 });
             dashboardProvider
                 .structure('6-6', {
@@ -898,6 +902,61 @@
             ];
 
         }])
+
+        //possible charts
+        .controller("charts", function ($scope) {
+
+            $scope.labels = ["January", "February", "March", "April", "May", "June", "July"];
+            $scope.series = ['Series A', 'Series B'];
+            $scope.data = [
+                [65, 59, 80, 81, 56, 55, 40],
+                [28, 48, 40, 19, 86, 27, 90]
+            ];
+            $scope.onClick = function (points, evt) {
+                console.log(points, evt);
+            };
+            $scope.datasetOverride = [{ yAxisID: 'y-axis-1' }, { yAxisID: 'y-axis-2' }];
+            $scope.options = {
+                scales: {
+                    yAxes: [
+                        {
+                            id: 'y-axis-1',
+                            type: 'linear',
+                            display: true,
+                            position: 'left'
+                        },
+                        {
+                            id: 'y-axis-2',
+                            type: 'linear',
+                            display: true,
+                            position: 'right'
+                        }
+                    ]
+                }
+            };
+
+            $scope.colors = ['#45b7cd', '#ff6384', '#ff8e72'];
+
+            $scope.labels = ['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday', 'Sunday'];
+            $scope.data = [
+                [65, -59, 80, 81, -56, 55, -40],
+                [28, 48, -40, 19, 86, 27, 90]
+            ];
+            $scope.datasetOverride = [
+                {
+                    label: "Bar chart",
+                    borderWidth: 1,
+                    type: 'bar'
+                },
+                {
+                    label: "Line chart",
+                    borderWidth: 3,
+                    hoverBackgroundColor: "rgba(255,99,132,0.4)",
+                    hoverBorderColor: "rgba(255,99,132,1)",
+                    type: 'line'
+                }
+            ];
+        })
 
         .run(['service', function (service) {
             service.getWeatherData();
